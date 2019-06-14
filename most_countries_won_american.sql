60dfd772dd672e15d5e7dd3e7b17afc5d0fcd4ce
nathandwyer <nathan.dwyer7@gmail.com>
2019-06-14 13:42:20 +0300

    Update most_countries_won_american.sql

--- most_countries_won_american.sql
+++ most_countries_won_american.sql
@@ -1,4 +1,4 @@
-```sql
+
 mysql> SELECT personid, personname, count(distinct competitions.countryid) AS gts 
 FROM results, competitions 
 WHERE roundtypeid IN ('c', 'f') 
@@ -8,4 +8,4 @@ AND competitions.id=results.competitionid
 GROUP BY personname 
 HAVING gts>1 
 ORDER BY gts DESC;
-```
+
